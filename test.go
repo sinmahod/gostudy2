b06fbc4e6109ff6b0b188c1be75b526bd47001ab
sinmahod <sinmahod@qq.com>
2016-09-11 18:53:04 +0800

    update

--- test.go
+++ test.go
@@ -2,23 +2,145 @@ package main
 
 import (
 	"bytes"
+	"crypto/sha256"
 	"fmt"
 	"math"
+	"path/filepath"
 	"strings"
 	"unicode"
 	"unicode/utf8"
 )
 
 func main() {
-	Test1()
+	Test9()
 }
 
-//测试，编写一个字符串大小写转换工具
-//测试编写一个输入/opt/qwe/test.go转换为test，com.qwe.test.go转换为com.qwe.test
+//练习 4.1： 编写一个函数，计算两个SHA256哈希码中不同bit的数目。（参考2.6.2节的PopCount函数。)
+func Test9() {
+	c1 := sha256.Sum256([]byte("x"))
+	c2 := sha256.Sum256([]byte("X"))
+	fmt.Println(c1)
+	fmt.Println(c2)
+	var it int = func() int {
+		mp := make(map[int]byte)
+		for i, cs2 := range c2 {
+			mp[i] = cs2
+		}
+		var i int
+		for _, cs1 := range c1 {
+			for k, v := range mp {
+				if cs1 == v {
+					fmt.Printf("存在重复的为%d\n", v)
+					i++
+					delete(mp, k)
+					break
+				}
+			}
+		}
+		return i
+	}()
+	fmt.Printf("%d", it)
+}
+
+//测试...声明数组：根据初始化的值数量确定数组大小
+func Test8() {
+	i := [...]int{1, 2, 3, 4}
+	fmt.Println(i, len(i))
+	s := [...]int{99: 1}
+	fmt.Println(s, len(s))
+	ptr := [...]byte{19: 1}
+	fmt.Println(ptr)
+	updateByte(&ptr) //如果直接传入指针的话会修改变量内容
+	fmt.Println(ptr)
+
+}
+
+func updateByte(ptrs *[20]byte) {
+	*ptrs = [20]byte{}
+}
+
+//练习 3.12： 编写一个函数，判断两个字符串是否是是相互打乱的，也就是说它们有着相同的字符，但是对应不同的顺序。
+func Test7() {
+	stra := "abcssas"
+	strb := "cbaasss"
+	if len(stra) != len(strb) {
+		fmt.Printf("%s与%s字符长度不一致\n", stra, strb)
+		return
+	}
+
+	mp := make(map[int]rune)
+
+	for _, s1 := range stra {
+		isexists := false
+		for i, s2 := range strb {
+			_, ok := mp[i] //判断这个元素是否已经使用过了
+			if s1 == s2 && !ok {
+				mp[i] = s2 //相同的元素放到map里
+				isexists = true
+				break
+			}
+		}
+		if !isexists {
+			fmt.Printf("%s与%s字符不一致\n", stra, strb)
+			return
+		}
+	}
+	fmt.Printf("%s与%s字符一致\n", stra, strb)
+}
+
+func Test6() {
+	st := comma("1234567")
+	fmt.Println(st)
+}
+
+func comma(s string) string {
+	var buf bytes.Buffer
+	con := len(s)
+	i := len(s)
+	for idx := 0; idx < len(s); idx++ {
+		i--
+		str := s[idx]
+		fmt.Println(str)
+		fmt.Fprintf(&buf, "%s", string(str))
+		if (idx+1)%3 == 0 && idx+1 < con {
+			buf.WriteString(",")
+		}
+
+	}
+	return buf.String()
+	// n := len(s)
+	// if n <= 3 {
+	// 	return s
+	// }
+	// return comma(s[:n-3]) + "," + s[n-3:]
+}
+
 //了解strings包的相关功能
+func Test1() {
+	//测试，字符串大小写转换工具
+	str := "hello,world!"
+	str = strings.ToUpper(str)
+	fmt.Println(str)
+	//测试编写一个输入/opt/qwe/test.go转换为test，com.qwe.test.go转换为com.qwe.test
+	fmt.Println(getFileName("com.qwe.test.go"))
+	fmt.Println(filepath.Split("/opt/qwe/test.go"))
+}
+
+func getFileName(str string) string {
+	idx := strings.LastIndex(str, "/")
+	idx2 := strings.LastIndex(str, ".")
+	str2 := str
+	if idx != -1 {
+		str2 = str2[idx+1:]
+	}
+	if idx2 != -1 {
+		str2 = str2[:idx2]
+	}
+	return str2
+}
 
 //测试字符串区字节值
-func Test1() {
+func Test2() {
 	str := "世22"
 	//fmt.Println(str[0:6] + str[0:6])
 	str2 := "\u4e1622"
